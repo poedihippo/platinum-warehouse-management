d187a5b4a692cd4985f710db32d59631d439bd4d
albatechnology <60589540+poedihippo@users.noreply.github.com>
2023-05-09 15:44:30 +0700

    [ADD] seeder for user, role and permission 2

--- app/Helpers/PermissionsHelper.php
+++ app/Helpers/PermissionsHelper.php
@@ -106,13 +106,12 @@ class PermissionsHelper
 
     public static function generateChilds(Permission $headSubPermissions, array $subPermissions)
     {
-        // $guard = 'sanctum';
-        $guard = '';
+        $guard = 'sanctum';
         collect($subPermissions)->each(function ($permission, $key) use ($headSubPermissions, $guard) {
             if (is_array($permission)) {
                 $hsp = Permission::firstOrCreate([
                     'name' => $key,
-                    // 'guard_name' => $guard,
+                    'guard_name' => $guard,
                     'parent_id' => $headSubPermissions->id
                 ]);
 
@@ -120,7 +119,7 @@ class PermissionsHelper
             } else {
                 $hsp = Permission::firstOrCreate([
                     'name' => $permission,
-                    // 'guard_name' => $guard,
+                    'guard_name' => $guard,
                     'parent_id' => $headSubPermissions->id
                 ]);
             }
