d3c8dc66a37df50cca549359afb65a509bce590a
albatechnology <60589540+poedihippo@users.noreply.github.com>
2023-08-23 18:01:41 +0700

    [FIX] adjust qty RO

--- app/Http/Requests/Api/ReceiveOrderDetailUpdateRequest.php
+++ app/Http/Requests/Api/ReceiveOrderDetailUpdateRequest.php
@@ -24,7 +24,7 @@ class ReceiveOrderDetailUpdateRequest extends FormRequest
      */
     public function rules()
     {
-        $receiveOrderDetail = $this->receive_order?->details()->where('id', $this->receive_order_detail)->firstOrFail();
+        $receiveOrderDetail = $this->receiveOrder?->details()->where('id', $this->receiveOrderDetail)->firstOrFail();
 
         return [
             'adjust_qty' => [
