5a397f863c9d6787f7fa359c4456bd6f379d55f7
Alba Tech <60589540+poedihippo@users.noreply.github.com>
2024-06-06 09:53:52 +0700

    Update TrustHosts.php

--- app/Http/Middleware/TrustHosts.php
+++ app/Http/Middleware/TrustHosts.php
@@ -15,6 +15,8 @@ class TrustHosts extends Middleware
     {
         return [
             $this->allSubdomainsOfApplicationUrl(),
+            'bejo.platinumadisentosa.com',
+            'localhost:3000',
         ];
     }
 }
