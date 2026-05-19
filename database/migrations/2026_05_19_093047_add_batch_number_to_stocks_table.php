f39b94bcf8c171dc94175d0bc155af98af529a56
Alba Tech <poedi1613@gmail.com>
2026-05-19 09:52:08 +0700

    Update 2026_05_19_093047_add_batch_number_to_stocks_table.php

--- database/migrations/2026_05_19_093047_add_batch_number_to_stocks_table.php
+++ database/migrations/2026_05_19_093047_add_batch_number_to_stocks_table.php
@@ -11,9 +11,6 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('stocks', function (Blueprint $table) {
-            $table->dropColumn(['batch_number']);
-        });
             Schema::table('stocks', function (Blueprint $table) {
                 $table->string('batch_number')->nullable()->after('parent_id');
                 $table->string('batch_number_jp')->nullable()->after('batch_number');
