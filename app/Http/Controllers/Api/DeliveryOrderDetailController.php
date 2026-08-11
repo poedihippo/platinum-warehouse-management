a491b3110f838463b659c2ffe97c49d680ebaaec
Alba Tech <poedi.maurif@hippo.id>
2026-08-11 14:08:06 +0700

    Update DeliveryOrderDetailController.php

--- app/Http/Controllers/Api/DeliveryOrderDetailController.php
+++ app/Http/Controllers/Api/DeliveryOrderDetailController.php
@@ -34,7 +34,7 @@ class DeliveryOrderDetailController extends Controller
                     'salesOrderDetail' => fn($q) => $q->select('id', 'sales_order_id', 'product_unit_id', 'fulfilled_qty', 'total_price')
                         ->with([
                             // 'warehouse',
-                            'salesOrder' => fn($q) => $q->select('id', 'invoice_no', 'resseler_id')
+                            'salesOrder' => fn($q) => $q->select('id', 'invoice_no', 'reseller_id')
                                 ->with('reseller', fn($q) => $q->select('id', 'name', 'phone', 'address')),
                             'productUnit' => fn($q) => $q->withTrashed()
                                 ->select('id', 'name', 'product_id', 'uom_id')
