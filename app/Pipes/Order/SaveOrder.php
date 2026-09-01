6ec2ceff073325836f8b1c36b08a588d2d93bbc6
Nikko Fe <60589540+poedihippo@users.noreply.github.com>
2026-09-01 10:47:00 +0700

    Update SaveOrder.php

--- app/Pipes/Order/SaveOrder.php
+++ app/Pipes/Order/SaveOrder.php
@@ -4,7 +4,6 @@ namespace App\Pipes\Order;
 
 use App\Enums\UserType;
 use App\Models\SalesOrder;
-use App\Models\SalesOrderDetail;
 use App\Models\User;
 use App\Services\SalesOrderService;
 use Illuminate\Support\Facades\DB;
@@ -40,9 +39,9 @@ class SaveOrder
             $salesOrder->save();
             $salesOrder->details()->saveMany($salesOrderDetails);
 
-            if ($salesOrder->is_invoice) {
-                $this->createSalesOrderItems($salesOrder, $salesOrderDetails);
-            }
+            // if ($salesOrder->is_invoice) {
+            //     $this->createSalesOrderItems($salesOrder, $salesOrderDetails);
+            // }
 
             return $salesOrder;
         });
@@ -65,70 +64,70 @@ class SaveOrder
         }
     }
 
-    private function createSalesOrderItems(SalesOrder $salesOrder, \Illuminate\Support\Collection $salesOrderDetails): void
-    {
-        $stockIdsByProductUnit = collect($salesOrder->raw_source['items'] ?? [])
-            ->map(fn ($item) => $item['stock_ids'] ?? [])
-            ->filter(fn ($stockIds) => ! empty($stockIds))
-            ->all();
+    // private function createSalesOrderItems(SalesOrder $salesOrder, \Illuminate\Support\Collection $salesOrderDetails): void
+    // {
+    //     $stockIdsByProductUnit = collect($salesOrder->raw_source['items'] ?? [])
+    //         ->map(fn ($item) => $item['stock_ids'] ?? [])
+    //         ->filter(fn ($stockIds) => ! empty($stockIds))
+    //         ->all();
 
-        $salesOrderDetails->each(function (SalesOrderDetail $salesOrderDetail) use ($stockIdsByProductUnit) {
-            $stockIds = $stockIdsByProductUnit[$salesOrderDetail->product_unit_id] ?? [];
+    //     $salesOrderDetails->each(function (SalesOrderDetail $salesOrderDetail) use ($stockIdsByProductUnit) {
+    //         $stockIds = $stockIdsByProductUnit[$salesOrderDetail->product_unit_id] ?? [];
 
-            if (! empty($stockIds)) {
-                $this->createScannedSalesOrderItems($salesOrderDetail, $stockIds);
-            } else {
-                $this->autoReserveSalesOrderItems($salesOrderDetail);
-            }
+    //         if (! empty($stockIds)) {
+    //             $this->createScannedSalesOrderItems($salesOrderDetail, $stockIds);
+    //         } else {
+    //             $this->autoReserveSalesOrderItems($salesOrderDetail);
+    //         }
 
-            SalesOrderService::countFulfilledQty($salesOrderDetail);
-        });
-    }
+    //         SalesOrderService::countFulfilledQty($salesOrderDetail);
+    //     });
+    // }
 
-    private function autoReserveSalesOrderItems(SalesOrderDetail $salesOrderDetail): void
-    {
-        $stocks = \App\Models\Stock::whereAvailableStock()
-            ->whereHas('stockProductUnit', fn ($q) => $q->where('product_unit_id', $salesOrderDetail->product_unit_id)->where('warehouse_id', $salesOrderDetail->warehouse_id))
-            ->limit($salesOrderDetail->qty)
-            ->get(['id'])->map(fn ($stock) => ['stock_id' => $stock->id]);
+    // private function autoReserveSalesOrderItems(SalesOrderDetail $salesOrderDetail): void
+    // {
+    //     $stocks = \App\Models\Stock::whereAvailableStock()
+    //         ->whereHas('stockProductUnit', fn ($q) => $q->where('product_unit_id', $salesOrderDetail->product_unit_id)->where('warehouse_id', $salesOrderDetail->warehouse_id))
+    //         ->limit($salesOrderDetail->qty)
+    //         ->get(['id'])->map(fn ($stock) => ['stock_id' => $stock->id]);
 
-        if ($stocks->count() < $salesOrderDetail->qty) {
-            throw new \Exception(sprintf('Stok %s tidak tersedia', $salesOrderDetail->productUnit->name), \Illuminate\Http\Response::HTTP_UNPROCESSABLE_ENTITY);
-        }
+    //     if ($stocks->count() < $salesOrderDetail->qty) {
+    //         throw new \Exception(sprintf('Stok %s tidak tersedia', $salesOrderDetail->productUnit->name), \Illuminate\Http\Response::HTTP_UNPROCESSABLE_ENTITY);
+    //     }
 
-        $salesOrderDetail->salesOrderItems()->createMany($stocks);
-    }
+    //     $salesOrderDetail->salesOrderItems()->createMany($stocks);
+    // }
 
-    private function createScannedSalesOrderItems(SalesOrderDetail $salesOrderDetail, array $stockIds): void
-    {
-        \App\Models\Stock::whereIn('id', $stockIds)->get(['id', 'parent_id'])->each(function ($stock) use ($salesOrderDetail) {
-            $this->createSalesOrderItemWithChildren($salesOrderDetail, $stock);
-        });
-    }
+    // private function createScannedSalesOrderItems(SalesOrderDetail $salesOrderDetail, array $stockIds): void
+    // {
+    //     \App\Models\Stock::whereIn('id', $stockIds)->get(['id', 'parent_id'])->each(function ($stock) use ($salesOrderDetail) {
+    //         $this->createSalesOrderItemWithChildren($salesOrderDetail, $stock);
+    //     });
+    // }
 
-    private function createSalesOrderItemWithChildren(SalesOrderDetail $salesOrderDetail, \App\Models\Stock $stock): void
-    {
-        $childIds = \App\Models\Stock::where('parent_id', $stock->id)->pluck('id');
+    // private function createSalesOrderItemWithChildren(SalesOrderDetail $salesOrderDetail, \App\Models\Stock $stock): void
+    // {
+    //     $childIds = \App\Models\Stock::where('parent_id', $stock->id)->pluck('id');
 
-        if ($childIds->isNotEmpty()) {
-            $parentItem = $salesOrderDetail->salesOrderItems()->create([
-                'stock_id' => $stock->id,
-                'is_parent' => true,
-            ]);
+    //     if ($childIds->isNotEmpty()) {
+    //         $parentItem = $salesOrderDetail->salesOrderItems()->create([
+    //             'stock_id' => $stock->id,
+    //             'is_parent' => true,
+    //         ]);
 
-            $childRows = $childIds->map(fn ($childId) => [
-                'stock_id' => $childId,
-                'parent_id' => $parentItem->id,
-            ])->all();
+    //         $childRows = $childIds->map(fn ($childId) => [
+    //             'stock_id' => $childId,
+    //             'parent_id' => $parentItem->id,
+    //         ])->all();
 
-            $salesOrderDetail->salesOrderItems()->createMany($childRows);
+    //         $salesOrderDetail->salesOrderItems()->createMany($childRows);
 
-            return;
-        }
+    //         return;
+    //     }
 
-        $salesOrderDetail->salesOrderItems()->create([
-            'stock_id' => $stock->id,
-            'is_parent' => false,
-        ]);
-    }
+    //     $salesOrderDetail->salesOrderItems()->create([
+    //         'stock_id' => $stock->id,
+    //         'is_parent' => false,
+    //     ]);
+    // }
 }
